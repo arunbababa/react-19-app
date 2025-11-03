858c59769a3781e4f2a3ad12c325b7299bebb07c
arunbababa <hatuki.1.gzs@icloud.com>
2025-11-03 13:17:19 +0900

    CHOICEのレコードタイプにundefinedを許容しlenterエラーをなくす→haskeyのプッシュ時エラーをなくす

--- src/types/UseQuiz.ts
+++ src/types/UseQuiz.ts
@@ -2,7 +2,7 @@ import { CHOICESType } from '@/types/UseChoices';
 
 export type Quiz = {
   CHOICES: CHOICESType;
-  handleUserChoice: (CHOICE: Record<string, boolean>) => void;
+  handleUserChoice: (CHOICE: Record<string, boolean | undefined>) => void;
   handleResult: () => void;
   result: string;
   userChoice: string;
